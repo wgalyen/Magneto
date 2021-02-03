3f76bce84e30d07db1f1a5a10ba015c9bd04d848
Warren Galyen <wgalyen@bellhelmets.com>
2021-02-03 12:56:24 -0800

    Fix mouse follow scroll

--- src/index.js
+++ src/index.js
@@ -275,7 +275,7 @@ export default class Magneto {
                 // Follow mouse
                 if (this.elementFollow.length > 0) {
                     this.elementFollow.forEach(function (element) {
-                        element.style.transform = 'translate3d(' + posMouse.x + 'px,' + posMouse.y + 'px, 0)';
+                        element.style.transform = 'translate3d(' + (posMouse.x - window.pageXOffset) + 'px,' + (posMouse.y - window.pageYOffset) + 'px, 0)';
                     });
                 }
 
